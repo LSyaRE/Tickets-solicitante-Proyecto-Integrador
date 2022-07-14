32f58f69e15b6bcdeea06dec96b036c206903570
LSyaRe <jonasvillacis@yahoo.com>
2022-07-14 13:13:27 -0500

    vaciar develop

--- Backend/src/main/java/yavirac/ticket_solicitante/features/respuesta/RespuestaRepository.java
+++
@@ -1,7 +0,0 @@
-package yavirac.ticket_solicitante.features.respuesta;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RespuestaRepository extends CrudRepository<Respuesta, Long> {
-    
-}
--- Backend/src/main/java/yavirac/ticket_solicitante/features/tickets_carrera/CarreraRepository.java
+++
@@ -1,7 +0,0 @@
-package yavirac.ticket_solicitante.features.tickets_carrera;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarreraRepository extends CrudRepository<Carrera, Long> {
-        
-    }
