f0d52b3aecd5f929cb93a50f423ed73ab1110326
valeriagalan <valeriagalan50@gmail.com>
2022-07-26 18:31:53 -0500

    Buscador

--- Backend/src/main/java/yavirac/ticket_solicitante/features/carrera/CarreraService.java
+++ Backend/src/main/java/yavirac/ticket_solicitante/features/carrera/CarreraService.java
@@ -1,5 +1,7 @@
 package yavirac.ticket_solicitante.features.carrera;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +28,14 @@ public class CarreraService {
 
     public void deleteById(long id){
         carreraRepository.deleteById(id);
+    }
+
+    public List<Carrera> findAll(){
+        return carreraRepository.findAll();
+    }
+
+    public List<Carrera> findByNombre(String term){
+        return carreraRepository.findByNombreLikeIgnoreCase(term+"%");
     }
     
 }
