e062c8bd9b3adef49037e24155cd89b76d0679e0
valeriagalan <valeriagalan50@gmail.com>
2022-07-25 20:46:27 -0500

    Link carrera

--- FrontEnd/src/app/layout/layout-routing.module.ts
+++ FrontEnd/src/app/layout/layout-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarreraComponent } from '../features/carrera/carrera.component';
+import { TicketComponent } from '../features/ticket/ticket.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainComponent } from './main/main.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
     children: [
       {path: '', component:DashboardComponent},
       {path: 'dashboard', component:DashboardComponent},
-      {path:'carrera-form',component:CarreraComponent}
+      {path:'carrera-form',component:CarreraComponent},
+      {path:'tiket',component:TicketComponent}
     ]
   }
 ];
